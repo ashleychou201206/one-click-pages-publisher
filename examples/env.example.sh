d64f5a49ffe65859a93ec050a5e39f3fe383d0ab
ashleychou201206 <ashleychou201206@gmail.com>
2026-04-21 15:24:10 +0800

    Add environment example

---
+++ examples/env.example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export SOURCE_DIR="$HOME/Desktop/html-reports"
+export TARGET_REPO="your-github-name/your-pages-repo"
+export TARGET_BRANCH="main"
+export TARGET_PATH="index.html"
+export KEYCHAIN_ACCOUNT="your-github-name"
+export KEYCHAIN_SERVICE="one-click-pages-publisher-token"
